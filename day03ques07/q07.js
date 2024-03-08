8b6c4aeb221b8b77edd2521df3f2212c7a47ad9c
Syeda Aleena <159823689+Syeda-Aleena@users.noreply.github.com>
2024-03-09 02:21:17 +0500

    Add files via upload

---
+++ day03ques07/q07.js
@@ -0,0 +1,20 @@
+// ADDITION
+var num1 = 4;
+var num2 = 4;
+var sum = (4 + 4);
+console.log(sum);
+// SUBSTRACTION
+var num3 = 10;
+var num4 = 2;
+var sub = (10 - 2);
+console.log(sub);
+// MULTIPLICATION
+var num5 = 2;
+var num6 = 4;
+var num = (2 * 4);
+console.log(num);
+// DIVISION
+var num7 = 16;
+var num8 = 2;
+var div = (16 / 2);
+console.log(div);
---
+++ day03ques07/q07.ts
@@ -0,0 +1,23 @@
+// ADDITION
+let num1: number = 4
+let num2: number = 4
+let sum: number =(4 + 4);
+console.log(sum)
+
+// SUBSTRACTION
+let num3: number = 10 
+let num4: number = 2
+let sub: number =(10 - 2);
+console.log(sub);
+
+// MULTIPLICATION
+let num5: number = 2
+let num6: number = 4
+let num: number= (2 * 4);
+console.log(num)
+
+// DIVISION
+let num7: number = 16 
+let num8: number = 2
+let div: number =(16 / 2);
+console.log(div)
